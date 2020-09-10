888a5c950c33e257a075e266b6e34613b4f272c3
pradhyumvyas <pradhyumvyas92@gmail.com>
2020-09-11 01:29:11 +0530

    Updates

---
+++ precache-manifest.e173daad5438f257ee2395389eaa5053.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3413571d6b38501d674d392508927299",
+    "url": "/First-React-Project/index.html"
+  },
+  {
+    "revision": "fb03bb449658713711d7",
+    "url": "/First-React-Project/static/css/main.1536b8be.chunk.css"
+  },
+  {
+    "revision": "065771622d07acb91f57",
+    "url": "/First-React-Project/static/js/2.1a2844ca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/First-React-Project/static/js/2.1a2844ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb03bb449658713711d7",
+    "url": "/First-React-Project/static/js/main.8cca828e.chunk.js"
+  },
+  {
+    "revision": "dc77eb8d475fe30f1acf",
+    "url": "/First-React-Project/static/js/runtime-main.d3a5ab79.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/First-React-Project/static/media/logo.5d5d9eef.svg"
+  }
+]);
